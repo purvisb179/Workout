13c6aa414a964dc51f3683f99b68ef55fef64318
Brandon Purvis <purvisb179@gmail.com>
2022-11-22 15:21:10 -0600

    clean enum

--- App/Enum/EnumExtensions.cs
+++ App/Enum/EnumExtensions.cs
@@ -14,4 +14,16 @@ public static class EnumExtensions
         var attrs = memInfo[0].GetCustomAttributes(typeof(DisplayName), false);
         return attrs.Length > 0 ? ((DisplayName)attrs[0]).Text : enumeration.ToString();
     }
+    
+    public static string GetMuscleGroup(this System.Enum enumeration)
+    {
+        var type = enumeration.GetType();
+        var memInfo = type.GetMember(enumeration.ToString());
+        
+        if (memInfo.Length <= 0) return enumeration.ToString();
+        var attrs = memInfo[0].GetCustomAttributes(typeof(DisplayName), false);
+        return attrs.Length > 0 ? ((DisplayName)attrs[0]).Text : enumeration.ToString();
+    }
+    
+    
 }
